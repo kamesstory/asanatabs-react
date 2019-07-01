97c3d8837254671a6e7d30756f0bad400f6c2520
Jason Wang <jhw513@gmail.com>
2019-06-30 22:53:29 -0700

    added todo message

--- plugin-src/background-scripts/background.js
+++ plugin-src/background-scripts/background.js
@@ -2,3 +2,6 @@ import 'chrome-extension-async';
 import { ExtensionServer } from './ExtensionServer.js';
 
 ExtensionServer.listen();
+
+// TODO: get rid of background script rerouting and only have one-way retrieval calls
+//  from tab page to the background script!
